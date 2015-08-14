7c0a2b6dea7105537a7f0beba9daf0c2cf094910
Conor Anstett <conoranstett@gmail.com>
2015-08-14 12:51:50 +0000

    new express line

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
-var app = express()
+var app = express.createServer(express.logger())
 
-var fs = require('fs');
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
