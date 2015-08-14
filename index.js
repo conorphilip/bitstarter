5f63bca1aa50d55179306af22c687905e64ab040
Conor Anstett <conoranstett@gmail.com>
2015-08-14 13:46:26 +0000

    more typo fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirnmame + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
     response.send("hello world, i guess")
